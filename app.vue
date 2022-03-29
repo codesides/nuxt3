e409505e1848f656baf057eb89a1de0f767d8cb1
codesides <tigerlijx@gmail.com>
2022-03-29 17:05:52 +0800

    init

---
+++ app.vue
@@ -0,0 +1,13 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-11-23 07:13:18
+ * @LastEditTime: 2022-03-15 16:16:27
+ * @LastEditors: codesides
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /nuxt3/app.vue
+-->
+<template>
+  <div>
+    <Asome />
+  </div>
+</template>
---
+++ components/Asome.vue
@@ -0,0 +1,55 @@
+<!--
+ * @Author: codesides
+ * @Descripttion: 
+ * @Date: 2022-03-15 15:41:51
+ * @LastEditors: codesides
+ * @LastEditTime: 2022-03-17 11:14:35
+ * @FilePath: /nuxt3/components/Asome.vue
+-->
+<template>
+  <div>
+    <button @click="change">change</button>
+    <iframe
+      ref="iframe"
+      src="http://10.80.102.112:8080/#?main_id=122278"
+      frameborder="0"
+      style="width: 1000px; height: 600px"
+    ></iframe>
+  </div>
+</template>
+
+<script lang='ts'>
+import { defineComponent, reactive, toRefs } from "vue";
+
+export default defineComponent({
+  name: "Asome",
+  props: {},
+  components: {},
+  setup() {
+    const a = ref(0);
+    return {
+      a,
+    };
+  },
+  methods: {
+    change() {
+      this.$refs.iframe.contentWindow.addMark("哈哈哈");
+    },
+    /**
+     * @description:
+     * @param {*} a
+     * @param {*} b
+     * @return {*}
+     */
+    test(a, b) {
+      return a + b;
+    },
+  },
+});
+</script>
+
+<style scoped>
+.a {
+  font-size: 14px;
+}
+</style>
